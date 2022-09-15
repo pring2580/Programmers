a0e5ce366d160327ee776a556a971b7217d9d92a
pring2580 <pring2580@gmail.com>
2022-09-15 16:53:20 +0900

    [프로그래머스 Lv.1] 문자열을 정수로 바꾸기

---
+++ src/Exam005.java
@@ -0,0 +1,17 @@
+/**
+ * 
+  * @FileName : Exam005.java
+  * @Project : Programmers
+  * @Date : 2022. 9. 15. 
+  * @작성자 : GnG
+  * @변경이력 : 
+  * @프로그램 설명 : [프로그래머스 Lv.1] 문자열을 정수로 바꾸기
+ */
+public class Exam005 {
+	public static void main(String[] args) {
+		String s = "+12345";
+		int answer = 0;
+		answer = Integer.parseInt(s);
+		System.out.println(answer);
+	}
+}
